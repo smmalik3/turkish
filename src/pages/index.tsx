7f477595b937f1d2622c6802aa7b05e62b491e25
Malik, Salman <salmalik@deloitte.com>
2024-12-29 18:24:57 -0500

    added toggles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,13 +16,23 @@ export default function Home() {
           <LanguageSwitcher currentLanguage={language} setLanguage={setLanguage} />
         </div>
         <div className="mb-6 flex items-center">
-          <label className="mr-2 text-lg font-medium text-gray-700">Show Images</label>
+          {/* <label className="mr-2 text-lg font-medium text-gray-700">Show Images</label>
           <input
             type="checkbox"
             checked={showImages}
             onChange={() => setShowImages(!showImages)}
             className="form-checkbox h-5 w-5 text-blue-600"
-          />
+          /> */}
+          <label className="inline-flex items-center cursor-pointer">
+            <input
+              type="checkbox"
+              checked={showImages}
+              onChange={() => setShowImages(!showImages)}
+              className="sr-only peer"
+            />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Show Images</span>
+          </label>
         </div>
         <div className="flex justify-center w-full">
           <div className="max-w-md w-full">
